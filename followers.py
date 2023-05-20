67cc56203d37b0cd6a8bee0e786069323a02e87d
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-05-20 13:30:03 +0100

    followers ready

--- followers.py
+++ followers.py
@@ -1,56 +1,43 @@
-from selenium import webdriver
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from time import sleep
+import requests
+import random
+import string
 
 
-# open username and password files
-with open('username.txt', 'r') as f:
-    usernames = f.read().splitlines()
-with open('password.txt', 'r') as f:
-    passwords = f.read().splitlines()
-
-# initialize a Chrome browser instance
-options = Options()
-options.add_experimental_option("detach", True)
-
-driver = webdriver.Chrome(options=options)
-
-# navigate to Spotify's login page
-driver.get('https://www.spotify.com/login/')
-
-# loop through the usernames and passwords and attempt to log in with each one
-for i in range(len(usernames)):
-    # enter the username and password
-
-    username = driver.find_element(By.ID, 'login-username')
-
-    password = driver.find_element(By.ID, 'login-password')
-    username.clear()
-    password.clear()
-    username.send_keys(usernames[i])
-    password.send_keys(passwords[i])
-
-    # click the login button
-    login_button = driver.find_element(By.ID, 'login-button')
-    login_button.click()
+def generate_random_string(length):
+    characters = string.ascii_lowercase + string.digits
+    return "".join(random.choice(characters) for _ in range(length))
 
 
+def follow_user(username, password, user_id):
+    # Authenticate with Spotify
+    auth_response = requests.post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'password',
+        'username': username,
+        'password': password,
+        'client_id': "8b4bf7a55fc54b9d81c86e7c17840f16",
+        'client_secret': "2af9f62f91d2493787432a1227bc87e2"
+    })
 
-sleep(5)
-driver.get('https://open.spotify.com/user/ahmeddanial')
+    if auth_response.status_code != 200:
+        print('Authentication failed')
+        return
 
+    access_token = auth_response.json().get('access_token')
 
-sleep(7)
-# click the follow button
-follow_button = driver.find_element(By.XPATH, '/html/body/div[4]/div/div[2]/div[4]/div[1]/div[2]/div[2]/div/div/div[2]/main/section/div/div[3]/div[4]/div/div/div/div/button[1]')
+    # Follow the user
+    follow_response = requests.put(f'https://api.spotify.com/v1/me/following?type=user&ids={user_id}', headers={
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    })
 
-if follow_button.text == 'FOLLOW':
-    follow_button.click()
-    print("Followed user ahmeddanial")
-else:
-    print("Already following user ahmeddanial")
+    if follow_response.status_code == 204:
+        print(f'Successfully followed user with ID: {user_id}')
+    else:
+        print('Failed to follow user')
 
-# close the browser window
-driver.quit()
+# Replace with your Spotify username, password, and the user ID you want to follow
+your_username = '31hdfr2p7t3pmsfwphl4g4t3p7te'
+your_password = '3q5cy3xfm3zo'
+user_to_follow_id = 'ahmeddanial'
+
+follow_user(your_username, your_password, user_to_follow_id)
