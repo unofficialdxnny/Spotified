8edb0d06200c4479b0a87deac845988bc4cb5cde
unofficialdxnny <ahmeddanial324@gmail.com>
2023-06-18 14:24:54 +0100

    update

--- follower.py
+++ follower.py
@@ -162,14 +162,14 @@ while True:
     
                     ## login
                     login_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div[1]/div[4]/button').click()
-                    sleep(4)
+                    sleep(2)
     
                     ## cookies
                     driver.find_element(By.XPATH, '/html/body/div[13]/div[3]/div/div[1]/div/div[2]/div/button[1]').click()
     
                     ## Search
                     driver.get(f'https://open.spotify.com/user/ahmeddanial')
-                    sleep(4)
+                    sleep(2)
     
                     ## Follow
                     driver.find_element(By.XPATH, "/html/body/div[4]/div/div[2]/div[4]/div[1]/div[2]/div[2]/div/div/div[2]/main/section/div/div[3]/div[4]/div/div/div/div/button[1]").click()
--- likes.py
+++ likes.py
@@ -185,7 +185,7 @@ while True:
                     
                     driver.quit()
                     # Change the terminal title to "followers | x" where x is the number of followers
-                    followers_count = followers_count+1  # Replace with the actual number of followers
+                    likes_count = followers_count+1  # Replace with the actual number of followers
                     new_title = f"followers | {followers_count}"
                     set_terminal_title(new_title)
     
